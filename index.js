aae4254c359fc0786c33d04c37686a9c4a846952
Mosharrof Hosen Munna <mdmunna31573@gmail.com>
2022-11-09 13:42:00 +0600

    create user api

--- index.js
+++ index.js
@@ -12,24 +12,39 @@ app.use(cors());
 app.use(express.json());
 
 app.get("/", (req, res) => {
-    res.send("Server is running");
-  });
-  const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@project.wytfk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+  res.send("Server is running");
+});
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@project.wytfk.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 
-  const client = new MongoClient(uri, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-  });
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
 
-  const run = async () => {
-    try {
-      
-    } finally {
-      // await client.close()
-    }
-  };
-  run().catch(console.dir);
-  
-  app.listen(PORT, () => {
-    console.log("server is running on port", PORT);
-  });
+const run = async () => {
+  try {
+    await client.connect();
+    const database = client.db("PickyPro");
+    const userCollections = database.collection("Users");
+
+    //   create new user
+    app.post("/api/user/create", async (req, res) => {
+      const userData = req.body;
+      const createdUser = await userCollections.insertOne(userData);
+      res.json(createdUser);
+    });
+
+    app.get('/api/user',async(req,res)=>{
+        const user = userCollections.find({})
+        const users = await user.toArray()
+        res.json(users)
+    })
+  } finally {
+    // await client.close()
+  }
+};
+run().catch(console.dir);
+
+app.listen(PORT, () => {
+  console.log("server is running on port", PORT);
+});
