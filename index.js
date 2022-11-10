ea73641a73ba4b2af90897210319076c49f7f866
Mosharrof Hosen Munna <mdmunna31573@gmail.com>
2022-11-10 15:21:59 +0600

    implement jwt for authorize user data

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const app = express();
 require("dotenv").config();
 const cors = require("cors");
 const ObjectId = require("mongodb").ObjectId;
+const  jwt = require('jsonwebtoken');
+const { verifyJWT } = require("./middleware");
 
 const PORT = process.env.PORT || 5000;
 
@@ -21,6 +23,8 @@ const client = new MongoClient(uri, {
   useUnifiedTopology: true,
 });
 
+
+
 const run = async () => {
   try {
     await client.connect();
@@ -34,7 +38,7 @@ const run = async () => {
       const userData = req.body;
       const createdUser = await userCollections.insertOne(userData);
       res.json(createdUser);
-    });
+    }); 
 
     // create an user google user
     app.put("/api/user/create", async (req, res) => {
@@ -50,6 +54,12 @@ const run = async () => {
       res.json(result);
     });
 
+    app.post('/api/jwt',async(req,res)=>{
+        const user = req.body
+        const token = jwt.sign(user,process.env.ACCESS_TOKEN_SECRET)
+        res.json({token})
+    })
+
     // service api
     app.post("/api/service/create", async (req, res) => {
       const serviceData = req.body;
@@ -63,7 +73,7 @@ const run = async () => {
       res.json(services);
     });
     app.get("/api/services/limit", async (req, res) => {
-      const cursor = serviceCollections.find({});
+      const cursor = serviceCollections.find({},{sort: {date:-1}});
       const services = await cursor.limit(3).toArray();
       res.json(services);
     });
@@ -71,19 +81,53 @@ const run = async () => {
     app.get("/api/service/:serviceId", async (req, res) => {
       const id = req.params.serviceId;
       const service = await serviceCollections.findOne({ _id: ObjectId(id) });
-      const cursor = reviewCollections.find({ serviceId: id });
+      const cursor = reviewCollections.find({ serviceId: id },{ sort: { time: -1 }});
       const reviews = await cursor.toArray();
-      res.json({service,reviews});
-    });
+      const reverseReviews =reviews
+      res.json({service, reviews: reverseReviews});
+    }); 
  
     //  review api *******************/////
 
-    app.post("/api/review/create", async (req, res) => {
+    app.post("/api/review/create",verifyJWT, async (req, res) => {
       const reviewData = req.body;
       const createdReview = await reviewCollections.insertOne(reviewData);
       const review = await reviewCollections.findOne({_id: ObjectId(createdReview.insertedId)})
-      res.json(review);
+      res.json(review); 
     });
+
+    // get all reviews for single user
+    app.get('/api/reviews/user/:uid', verifyJWT, async(req,res)=>{
+        const uid = req.params.uid
+
+        if(req.user?.uid !== uid){
+            return res.json({message: 'unauthorized'})
+        }
+ 
+        const cursor = reviewCollections.find({"author.uid":uid},{ sort: { time: -1 }})
+        const reviews = await cursor.toArray()
+        res.json(reviews) 
+    })
+
+    app.put('/api/review/update/:reviewId',async(req,res)=>{
+        const reviewId = req.params.reviewId
+        const reviewData  = req.body
+        const filter = {_id:ObjectId(reviewId)}
+        const updateDoc = {
+            $set: reviewData
+          };
+          const result = await reviewCollections.updateOne(filter,updateDoc)
+          const newReview = await reviewCollections.findOne(filter)
+          res.json(newReview)
+
+    })
+
+    app.delete('/api/review/delete/:reviewId',async(req,res)=>{
+        const reviewId = req.params.reviewId
+        const filter = {_id: ObjectId(reviewId)}
+        const result = await reviewCollections.deleteOne(filter)
+        res.json(result)
+    }) 
 
     
 
